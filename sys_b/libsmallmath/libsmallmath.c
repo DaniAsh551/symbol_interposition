d906ad4cda749ca34ccc4dac43d77628085d064d
d4n1 <d4n1.551@gmail.com>
2023-11-26 21:41:16 +0500

    sys_b: libsmallmath

---
+++ sys_b/libsmallmath/libsmallmath.c
@@ -0,0 +1,10 @@
+#include "libsmallmath.h"
+#include <math.h>
+
+float m_add(float a, float b) {
+    return a + b;
+}
+
+float m_sub(float a, float b) {
+    return a - b;
+}
---
+++ sys_b/libsmallmath/libsmallmath.h
@@ -0,0 +1,8 @@
+#ifndef LIBSMALLMATH_H
+#define LIBSMALLMATH_H
+
+// Function declarations for system_a's libsmallmath
+float m_add(float a, float b);
+float m_sub(float a, float b);
+
+#endif  // LIBSMALLMATH_H
